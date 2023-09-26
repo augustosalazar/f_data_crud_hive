fae99bf4587ce1fc8b3dea6a6fc0510270b3466c
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2023-09-26 12:25:03 -0500

    include instruction

--- lib/data/models/some_data_db.dart
+++ lib/data/models/some_data_db.dart
@@ -2,6 +2,8 @@ import 'package:hive/hive.dart';
 
 part 'some_data_db.g.dart';
 
+//execute dart run build_runner build
+
 @HiveType(typeId: 1)
 class SomeDataDb extends HiveObject {
   SomeDataDb({required this.name, required this.description});
